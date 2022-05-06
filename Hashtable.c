5cc31658acf2f5ec9e83cadfa46959bf796d6aab
Charlie Vieth <charlie.vieth@gmail.com>
2022-05-05 20:17:39 -0400

    WIP 2

--- Hashtable.c
+++ Hashtable.c
@@ -27,6 +27,14 @@ in the source distribution for its full text.
 
 typedef struct HashtableItem_ {
    ht_key_t key;
+   // NOTE: the below is true, but would require 4 billion entries
+   // which is: unlikely and would require a truly insane amount of
+   // memory. Also, not sure any OS can support 4 billion procs.
+   //
+   // TODO: this can *maybe* be a uint32_t. The largest key is
+   // UINT32_MAX but there is a pathological case where we store
+   // UINT32_MAX values and thus the map size is larger than MAX
+   // and we end up with a very very long probe.
    size_t probe;
    void* value;
 } HashtableItem;
@@ -41,30 +49,88 @@ struct Hashtable_ {
 
 
 #ifndef NDEBUG
+#include <stdarg.h>
+#include <stdbool.h>
 
-static void Hashtable_dump(const Hashtable* this) {
-   fprintf(stderr, "Hashtable %p: size=%zu items=%zu owner=%s\n",
+FILE *hash_debug_file = NULL;
+
+static FILE* init_hash_debug_file() {
+   if (hash_debug_file == NULL) {
+      FILE *fp = fopen("debug_hash.txt", "a");
+      if (!fp) {
+         CRT_fatalError("cannot open hash debug file: debug_hash.txt");
+         abort();
+      }
+      fprintf(fp, "\n######################################################\n");
+      fprintf(fp, "######################################################\n\n");
+      hash_debug_file = fp;
+   }
+   return hash_debug_file;
+}
+
+ATTR_FORMAT(printf, 1, 2)
+static void debugf(const char *fmt, ...) {
+   va_list args1, args2;
+   va_start(args1, fmt);
+   va_copy(args2, args1);
+
+   va_list args[2] = {args1, args2};
+   FILE *dfp = init_hash_debug_file();
+   FILE *fps[2] = {dfp, stderr};
+
+   for (int i = 0; i < 2; i++) {
+      FILE *fp = fps[i];
+      if (!fp) {
+         fprintf(stderr, "WARN: Hashtable: NULL file at index: %d\n", i);
+         continue;
+      }
+      int ret = vfprintf(fp, fmt, args[i]);
+      if (ret < 0) {
+         fprintf(stderr, "ERROR: debug(\"%s\") returned: %d\n", fmt, ret);
+         abort();
+      }
+   }
+   va_end(args1);
+   va_end(args2);
+   fflush(dfp);
+}
+
+static void Hashtable_dump(const Hashtable* this, bool dump_values) {
+   size_t max_probe = 0;
+   for (size_t i = 0; i < this->size; i++) {
+      if (this->buckets[i].value && this->buckets[i].probe > max_probe) {
+         max_probe = this->buckets[i].probe;
+      }
+   }
+   // size_t alloc_size = sizeof(HashtableItem) * this->size;
+   double mbs = (double)(sizeof(HashtableItem) * this->size) / (double)(1024 * 1024);
+   debugf("Hashtable %p: size=%zu items=%zu shrink_requests=%"PRIu32" owner=%s MBs=%.2f max_probe=%zu\n",
            (const void*)this,
            this->size,
            this->items,
-           this->owner ? "yes" : "no");
+           this->shrink_requests,
+           this->owner ? "yes" : "no",
+           mbs,
+           max_probe);
 
-   size_t items = 0;
-   for (size_t i = 0; i < this->size; i++) {
-      fprintf(stderr, "  item %5zu: key = %5u probe = %2zu value = %p\n",
-              i,
-              this->buckets[i].key,
-              this->buckets[i].probe,
-              this->buckets[i].value ? (const void*)this->buckets[i].value : "(nil)");
+   if (dump_values) {
+      size_t items = 0;
+      for (size_t i = 0; i < this->size; i++) {
+         debugf("  item %5zu: key = %5"PRIu32" probe = %2zu value = %p\n",
+                 i,
+                 this->buckets[i].key,
+                 this->buckets[i].probe,
+                 (const void*)this->buckets[i].value);
 
-      if (this->buckets[i].value)
-         items++;
+         if (this->buckets[i].value)
+            items++;
+      }
+
+      debugf("Hashtable %p: items=%zu counted=%zu\n",
+              (const void*)this,
+              this->items,
+              items);
    }
-
-   fprintf(stderr, "Hashtable %p: items=%zu counted=%zu\n",
-           (const void*)this,
-           this->items,
-           items);
 }
 
 static bool Hashtable_isConsistent(const Hashtable* this) {
@@ -75,7 +141,7 @@ static bool Hashtable_isConsistent(const Hashtable* this) {
    }
    bool res = items == this->items;
    if (!res)
-      Hashtable_dump(this);
+      Hashtable_dump(this, true);
    return res;
 }
 
@@ -91,6 +157,8 @@ size_t Hashtable_count(const Hashtable* this) {
 
 #endif /* NDEBUG */
 
+#define MIN_TABLE_SIZE 11
+
 /* Primes borrowed from gnulib/lib/gl_anyhash_primes.h.
 
    Array of primes, approximately in steps of factor 1.2.
@@ -98,18 +166,19 @@ size_t Hashtable_count(const Hashtable* this) {
      (dotimes (i 244) (format t "nextprime(~D)~%" (ceiling (expt 1.2d0 i))))
    and feeding the result to PARI/gp. */
 static const size_t primes[] = {
-   11, 13, 17, 19, 23, 29, 37, 41, 47, 59, 67, 83, 97, 127, 139, 167, 199, 239,
-   293, 347, 419, 499, 593, 709, 853, 1021, 1229, 1471, 1777, 2129, 2543, 3049,
-   3659, 4391, 5273, 6323, 7589, 9103, 10937, 13109, 15727, 18899, 22651,
-   27179, 32609, 39133, 46957, 56359, 67619, 81157, 97369, 116849, 140221,
-   168253, 201907, 242309, 290761, 348889, 418667, 502409, 602887, 723467,
-   868151, 1041779, 1250141, 1500181, 1800191, 2160233, 2592277, 3110741,
-   3732887, 4479463, 5375371, 6450413, 7740517, 9288589, 11146307, 13375573,
-   16050689, 19260817, 23112977, 27735583, 33282701, 39939233, 47927081,
-   57512503, 69014987, 82818011, 99381577, 119257891, 143109469, 171731387,
-   206077643, 247293161, 296751781, 356102141, 427322587, 512787097, 615344489,
-   738413383, 886096061, 1063315271, 1275978331, 1531174013, 1837408799,
-   2204890543UL, 2645868653UL, 3175042391UL, 3810050851UL,
+   MIN_TABLE_SIZE, 13, 17, 19, 23, 29, 37, 41, 47, 59, 67, 83, 97, 127, 139,
+   167, 199, 239, 293, 347, 419, 499, 593, 709, 853, 1021, 1229, 1471, 1777,
+   2129, 2543, 3049, 3659, 4391, 5273, 6323, 7589, 9103, 10937, 13109, 15727,
+   18899, 22651, 27179, 32609, 39133, 46957, 56359, 67619, 81157, 97369,
+   116849, 140221, 168253, 201907, 242309, 290761, 348889, 418667, 502409,
+   602887, 723467, 868151, 1041779, 1250141, 1500181, 1800191, 2160233,
+   2592277, 3110741, 3732887, 4479463, 5375371, 6450413, 7740517, 9288589,
+   11146307, 13375573, 16050689, 19260817, 23112977, 27735583, 33282701,
+   39939233, 47927081, 57512503, 69014987, 82818011, 99381577, 119257891,
+   143109469, 171731387, 206077643, 247293161, 296751781, 356102141, 427322587,
+   512787097, 615344489, 738413383, 886096061, 1063315271, 1275978331,
+   1531174013, 1837408799, 2204890543UL, 2645868653UL, 3175042391UL,
+   3810050851UL,
    /* on 32-bit make sure we do not return primes not fitting in size_t */
 #if SIZE_MAX > 4294967295ULL
    4572061027ULL, 5486473229ULL, 6583767889ULL, 7900521449ULL, 9480625733ULL,
@@ -214,7 +283,7 @@ static void insert(Hashtable* this, ht_key_t key, void* value) {
 
 void Hashtable_setSize(Hashtable* this, size_t size) {
 
-   assert(size > this->items);
+   assert(size == 0 || size > this->items);
    assert(Hashtable_isConsistent(this));
 
    size_t newSize = nextPrime(size);
@@ -250,7 +319,8 @@ void Hashtable_setSize(Hashtable* this, size_t size) {
 /* USABLE_FRACTION is the maximum dictionary load.
  * Currently set to 2/3 capacity.
  */
-#define USABLE_FRACTION(n) (((n) << 1)/3)
+// #define USABLE_FRACTION(n) (((n) << 1)/3)
+#define USABLE_FRACTION(n) (((n) << 2)/5)
 
 /* GROWTH_RATE. Growth rate upon hitting maximum load.
  * Currently set to items*3.
@@ -271,72 +341,33 @@ void Hashtable_setSize(Hashtable* this, size_t size) {
  * If the GROWTH_RATE of the hash exceeds the hash's capacity we reset the
  * shrink_requests counter since shrinking is no longer possible.
  */
-#define SHRINK_THRESHOLD(h) (((h)->size < SIZE_MAX ? (h)->size + 1 : SIZE_MAX) / 2)
+
+#define SHRINK_THRESHOLD(n) (((n) << 1)/8)
+
 
 static inline void Hashtable_insertResize(Hashtable* this) {
    const size_t size = this->size;
-   const size_t min_size = primes[0];
+   const size_t items = this->items;
 
 #ifndef NDEBUG
    Hashtable_dump(this, false);
 #endif
 
    /* grow the hash table */
-   if (this->items >= USABLE_FRACTION(size)) {
+   if (items >= USABLE_FRACTION(size)) {
       Hashtable_setSize(this, GROWTH_RATE(this));
-
-   /* shrinking no longer possible, reset the shrink request count */
-   } else if (GROWTH_RATE(this) >= size && this->shrink_requests > 0) {
-      this->shrink_requests = 0;
-
-   // TODO: avoid shrinking small hash tables
+      return;
+   }
 
    /* check if we should shrink the hash table */
-   } else if (size > min_size && GROWTH_RATE(this) < USABLE_FRACTION(size)) {
+   if (MIN_TABLE_SIZE < size && items <= SHRINK_THRESHOLD(size)) {
 #ifndef NDEBUG
       debugf("Hashtable: shrinking: size: %zu => %zu items: %zu\n",
          size, GROWTH_RATE(this), this->items);
 #endif
       Hashtable_setSize(this, GROWTH_RATE(this));
-      // uint32_t reqs = ++this->shrink_requests;
-      // if (reqs >= SHRINK_THRESHOLD(this) || reqs == UINT32_MAX) {
-      //    /* we're consistently below the usable capacity so shrink */
-      //    Hashtable_setSize(this, GROWTH_RATE(this));
-      // }
    }
 }
-
-// static inline void Hashtable_insertResize_XXX(Hashtable* this) {
-//    const size_t size = this->size;
-//
-//    /* grow the hash table */
-//    if (this->items >= USABLE_FRACTION(size)) {
-//       Hashtable_setSize(this, GROWTH_RATE(this));
-//       return;
-//    }
-//
-//    // TODO: note this is around 50% of capacity
-//
-//    /* shrinking no longer possible, reset the shrink request count */
-//    if (GROWTH_RATE(this) >= size && this->shrink_requests > 0) {
-//       this->shrink_requests = 0;
-//       return;
-//    }
-//
-//    /* avoid shrinking small hash tables */
-//    if (size < 64) {
-//       return;
-//    }
-//
-//    /* check if we should shrink the hash table */
-//    if (GROWTH_RATE(this) < USABLE_FRACTION(size)) {
-//       uint32_t reqs = ++this->shrink_requests;
-//       if (reqs >= SHRINK_THRESHOLD(this) || reqs == UINT32_MAX) {
-//          /* we're consistently below the usable capacity so shrink */
-//          Hashtable_setSize(this, GROWTH_RATE(this));
-//       }
-//    }
-// }
 
 void Hashtable_put(Hashtable* this, ht_key_t key, void* value) {
 
@@ -431,6 +462,27 @@ void* Hashtable_get(const Hashtable* this, ht_key_t key) {
 
    return res;
 }
+
+// void* Hashtable_get_XXX(const Hashtable* this, ht_key_t key) {
+//    size_t index = key % this->size;
+//    size_t probe = 0;
+//
+//    HashtableItem *buckets = this->buckets;
+//    const HashtableItem *b = &buckets[index];
+//    const HashtableItem *end = &buckets[this->size - 1];
+//
+//    while (b->value) {
+//       if (b->key == key) {
+//          return b->value;
+//       }
+//       if (b->probe < probe) {
+//          break;
+//       }
+//       b = b < end ? b + 1 : buckets;
+//       probe++;
+//    }
+//    return NULL;
+// }
 
 void Hashtable_foreach(Hashtable* this, Hashtable_PairFunction f, void* userData) {
    assert(Hashtable_isConsistent(this));
